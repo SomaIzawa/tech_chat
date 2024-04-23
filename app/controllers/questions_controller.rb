595cb190c46ef67f3f76584ac60a4dc4238d4c38
PL2304A1111 <pl2304a1111@PL2304A1111--m.local>
2024-04-23 13:36:54 +0900

    コメント投稿機能とその周辺機能を追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,8 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:edit, :show]
+
   def index
-    @questions = Question.includes(:user)
+    @questions = Question.includes(:user).includes(:comments)
   end
 
   def new
@@ -12,8 +14,17 @@ class QuestionsController < ApplicationController
     redirect_to '/'
   end
 
+  def show
+    @comment = Comment.new
+    @comments = @question.comments.includes(:user)
+  end
+
   private
   def question_params
     params.require(:question).permit(:title, :content, :name)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "questions#index"
-  resources :questions, only: [:index, :new, :create]
+  resources :questions, only: [:index, :new, :create, :show]
+  resources :questions do
+    resources :comments, only: [:index, :create]
+  end
 end
